553be68cded7b1ddae9f162b8fd633ec43d1bddc
Lluis Espert Cuquerella <lluisespertc@gmail.com>
2025-03-01 09:40:06 +0100

    Actualizado Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-navbar">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Serrano Sistemas</a>
+        <a className="navbar-brand" href="../ubxsx.html">Serrano Sistemas</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
